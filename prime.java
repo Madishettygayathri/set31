d16c5df572681eac954b6b1a940cf2693a10848e
Madishettygayathri <madishettygayathri94@gmail.com>
2024-11-27 14:44:15 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,7 +1,7 @@
 class prime{
     public static void main(String[]args)
     {
-        int n=23,flag=0;
+        int n=41,flag=0;
         for(int i=2;i<=Math.sqrt(n);i++)
         {
             if(n%i==0)
